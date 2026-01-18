30aeca87baf85836786fd07f961f129569c8a877
Emil Chigu <60775699+EmilChigu@users.noreply.github.com>
2026-01-18 01:24:41 +0000

    feat: create join chat form

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,6 +1,51 @@
 <script setup lang="ts">
+import { Head, router, useForm } from '@inertiajs/vue3';
+import { computed } from 'vue';
+
+defineProps({ errors: Object });
+
+// TODO: move to separate file
+interface IJoinChatForm {
+    username: string;
+}
+
+const form = useForm<IJoinChatForm>({
+    username: '',
+});
+
+// TODO: add proper validation with veevalidate + yup
+const isValid = computed(() => form.username && form.username.length > 3);
+
+function submit() {
+    router.post('/', {username:form.username});
+}
+
 </script>
 
 <template>
-    <div>Clean</div>
+    <Head title="Welcome | The Anonymous Team Chat" />
+    <div class="flex h-screen items-center justify-center bg-chat-bg p-6">
+        <div class="rounded-base bg-white p-5 shadow-lg sm:p-8">
+            <div class="flex flex-col gap-3">
+                <h1 class="text-center text-3xl font-bold">Signable Chat</h1>
+                <p class="">Real-time vibes, someone will probably steal your cool username though.</p>
+            </div>
+
+            <div class="mt-6 sm:mt-12">
+                <form @submit.prevent="submit" class="space-y-6">
+                    <div>
+                        <label class="mb-2"> What should we call you this time? </label>
+                        <input type="text" placeholder="e.g. R2D2" class="input" autofocus v-model="form.username" id="username" />
+                        <div v-if="errors?.username" class="mt-2 px-1 text-sm font-medium text-error">
+                            {{ errors.username }}
+                        </div>
+                    </div>
+
+
+<!--                    TODO: Add loading spinner-->
+                    <button type="submit" class="btn-primary" :disabled="form.processing || !isValid">Start Chatting</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
